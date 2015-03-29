db53664a0a3966dcb657c2e25daf3ee6a24a8f93
Jan <jan@position-absurd.de>
2015-03-29 19:42:20 +0200

    form icons adde

--- Mold/GUI/Lawn/Components/FormInput.js
+++ Mold/GUI/Lawn/Components/FormInput.js
@@ -19,89 +19,110 @@ Seed({
 	},
 	function(node, element, collection){
 
-		var hasAbreastParent = element.parent('[has~="abreast-labels"]');
-		
-		var highlighter = new Mold.Lib.Element("div");
+		var type = element.attr("type");
 
+		if(type !== "radio" && type !== "checkbox"){
+			
+			var hasAbreastParent = element.parent('[has~="abreast-labels"]'),
+				highlighter = new Mold.Lib.Element("div"),
+				errorMessage = new Mold.Lib.Element("span"),
+				parentColor = element.parent().css("color", ":before");
 
-		
-		element.after(highlighter);
+			errorMessage.setAttribute("is", "form-error");
 
-		var initHeightlighter = function(){
-			console.log("init highlighter");
+			element.after(highlighter);
+			highlighter.after(errorMessage);
 
-			var position = element.position(),
-				sizes = element.sizes();
+			var initHeightlighter = function(){
 
-
-			highlighter.css({
-				height : "3px",
-				width : sizes.width + "px",
-				position : "absolute",
-				backgroundColor : Colors.primaryBackground,
-				top : (position.top + sizes.height - (+element.css("marginBottom").replace("px", "")))+ "px",
-				left : position.left + "px",
-				transform : "scale3d(0, 1, 1)"
-			});
-		}
-
-		var doc = new Mold.Lib.Element(window);
-
-		doc.on("afterresize", function(){
-			initHeightlighter();
-		});
-
-		doc.on("beforeresize", function(){
-			highlighter.css({
-				transform : "scale3d(0, 1, 1)"
-			});
-		})
-
-
-
-		initHeightlighter();
-
-		element.on("focus", function(){
-			highlighter.animate({
-				transform : "scale3d(1, 1, 1)"
-			}, 0.4)
-		});
-
-		element.on("blur", function(){
-
-			highlighter.animate({
-				transform :  "scale3d(0, 1, 1)"
-			}, 0.4)
-		})
-
-		if(!hasAbreastParent){
-	
-			var label = element.previous();
-			if(label.nodeName.toLowerCase() === "label"){
-				
-				label.css({
-					"opacity" : "0",
-					"position" : "relative",
-					"bottom" : "-5px"
+				var position = element.position(true),
+					sizes = element.sizes();
+				highlighter.css({
+					height : "3px",
+					width : sizes.width + "px",
+					position : "absolute",
+					backgroundColor : Colors.primaryBackground,
+					top : (position.top + sizes.height - element.css("marginBottom").replace("px", ""))+ "px",
+					left : position.left + "px",
+					transform : "scale3d(0, 1, 1)"
 				});
-
-				element.on("focus", function(){
-					label.animate({
-						"opacity" : "1",
-						"bottom" : "0px"
-					}, 0.2, 'ease-in', 0.2)
-				})
-
-				element.on("blur", function(){
-					label.animate({
-						"opacity" : "0",
-						"bottom" : "-5px"
-					}, 0.2, 'ease-in', 0)
-				})
 			}
-			//element.on("focuse")
-		}
 
+			var doc = new Mold.Lib.Element(window);
+
+			doc.on("afterresize", function(){
+				initHeightlighter();
+			});
+
+			doc.on("beforeresize", function(){
+				highlighter.css({
+					transform : "scale3d(0, 1, 1)"
+				});
+			})
+
+			initHeightlighter();
+
+
+			var checkValidation = function(){	
+				if(!element.validity.valid){
+					errorMessage.val(element.validationMessage);
+				}else{
+					if(errorMessage){
+						errorMessage.val("");
+					}
+				}
+			}
+
+			element.on("focus", function(){
+				initHeightlighter();
+			
+				element.parent().css("color", Colors.primaryBackground, ":before");
+				highlighter.animate({
+					transform : "scale3d(1, 1, 1)"
+				}, 0.4)
+			});
+
+
+			element.on("blur", function(){
+				checkValidation();
+				element.parent().css("color", parentColor, ":before");
+				highlighter.animate({
+					transform :  "scale3d(0, 1, 1)"
+				}, 0.4)
+			})
+
+			element.on("keyup", function(){
+				checkValidation();
+			})
+
+			if(!hasAbreastParent){
+		
+				var label = element.previous();
+				if(label.nodeName.toLowerCase() === "label"){
+					
+					label.css({
+						"opacity" : "0",
+						"position" : "relative",
+						"bottom" : "-5px"
+					});
+
+					element.on("focus", function(){
+						label.animate({
+							"opacity" : "1",
+							"bottom" : "0px"
+						}, 0.2, 'ease-in', 0.2)
+					})
+
+					element.on("blur", function(){
+						label.animate({
+							"opacity" : "0",
+							"bottom" : "-5px"
+						}, 0.2, 'ease-in', 0)
+					})
+				}
+				//element.on("focuse")
+			}
+		}
 
 	}
 )
